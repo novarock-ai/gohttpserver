0e379bf0cc717e8dc924fdd6b95bcfccaf8181d8
DingShinn <dingyubo@dp.tech>
2023-06-13 20:53:46 +0800

    update: support custom ras path

--- common/constants.go
+++ common/constants.go
@@ -1,6 +1,20 @@
 package common
 
+import "os"
+
 const YAMLCONF = ".ghs.yml"
 const SecretPath = "./var/secret"
-const PublicKeyPath = SecretPath + "/public.pem"
-const PrivateKeyPath = SecretPath + "/private.pem"
+
+var PublicKeyPath = SecretPath + "/public.pem"
+var PrivateKeyPath = SecretPath + "/private.pem"
+
+func init() {
+	customPublicKeyPath := os.Getenv("PUBLIC_KEY_PATH")
+	if customPublicKeyPath != "" {
+		PublicKeyPath = customPublicKeyPath
+	}
+	customPrivateKeyPath := os.Getenv("PRIVATE_KEY_PATH")
+	if customPrivateKeyPath != "" {
+		PrivateKeyPath = customPrivateKeyPath
+	}
+}
